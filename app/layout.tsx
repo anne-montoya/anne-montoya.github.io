d1bc088a04c2940914532d46d713170650e376a4
Anne Montoya <annekristinemontoya@gmail.com>
2024-07-24 03:59:41 +0800

    google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
             <head>
                 <title>Anne Montoya | Software Engineer</title>
                 <link rel="icon" href="/logo-no-background.svg"/>
+                <meta name="google-site-verification" content="bdKxrDaiE_uT8pCC4cJGS8DmLaEluhfXUYbYi6jbMZA" />
             </head>
             <body>
                 <Navigation />
